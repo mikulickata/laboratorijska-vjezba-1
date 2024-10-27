5b6cd698cb99b40bbf835813cf414a5886a90759
mikulickata <142401283+mikulickata@users.noreply.github.com>
2024-10-27 21:24:02 +0100

    Add base-url

--- lab1-backend/src/services/ticketService.ts
+++ lab1-backend/src/services/ticketService.ts
@@ -3,22 +3,22 @@ import pool from '../database/database';
 import { generateQRCode } from './qrCodeService';
 
 export const generateTicket = async (vatin: string, firstName: string, lastName: string, token: string | undefined) => {
-  console.log("generateTicket")
+  console.log("generateTicket");
   const id = uuidv4();
-  const ticketUrl = `http://localhost:3000/tickets/${id}`;  // Generate the ticket URL
+  const ticketUrl = `${process.env.BASE_URL}/tickets/${id}`;  // Generate the ticket URL dynamically
 
   const result = await pool.query('SELECT COUNT(*) FROM tickets WHERE vatin = $1', [vatin]);
   if (parseInt(result.rows[0].count) >= 3) {
-    throw { statusCode: 400, message: 'Maksimalan broj ulaznica je već generiran za ovaj OIB' };
+     throw { statusCode: 400, message: 'Maksimalan broj ulaznica je već generiran za ovaj OIB' };
   }
-  console.log("result: ", result)
+
   const query = 'INSERT INTO tickets (id, vatin, first_name, last_name, created_at) VALUES ($1, $2, $3, $4, NOW()) RETURNING *';
   const values = [id, vatin, firstName, lastName];
-
   const newTicket = await pool.query(query, values);
+
   const qrCodeUrl = await generateQRCode(ticketUrl); // Generate the QR code
-  console.log("ticket-url: ", ticketUrl)
-  return { ...newTicket.rows[0], ticketUrl, qrCodeUrl, token}; // Include QR code URL in the response
+  console.log("ticket-url: ", ticketUrl);
+  return { ...newTicket.rows[0], ticketUrl, qrCodeUrl, token }; // Include QR code URL in the response
 };
 
 export const getTicketDetails = async (id: string) => {
