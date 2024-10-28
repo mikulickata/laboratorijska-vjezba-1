3f690dba7a7c433e3b17b2b57de486797abb2c2c
mikulickata <142401283+mikulickata@users.noreply.github.com>
2024-10-28 03:22:14 +0100

    Add css

--- lab1-frontend/src/pages/Home.tsx
+++ lab1-frontend/src/pages/Home.tsx
@@ -1,7 +1,7 @@
 // src/pages/Home.tsx
 import React, { useEffect, useState } from 'react';
 import { fetchTicketCount } from '../services/apiService'; // Import the API function
-import './styles/Home.css'; // Add this line at the top of the file
+import '../styles/Home.css'; // Add this line at the top of the file
 
 const Home: React.FC = () => {
   const [count, setCount] = useState<number>(0);
